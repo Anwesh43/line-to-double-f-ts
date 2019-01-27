75fbba519fc9c2318f0b81a4dd7eade641a74991
Anwesh43 <anweshthecool0@gmail.com>
2019-01-27 20:39:31 +0530

    adding LineToDoubleF class

--- LineToDoubleF.ts
+++ LineToDoubleF.ts
@@ -185,3 +185,27 @@ class LTDNode {
         return this
     }
 }
+
+class LineToDoubleF {
+
+    root : LTDNode = new LTDNode(0)
+    curr : LTDNode = this.root
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.root.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
