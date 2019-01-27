d5e350e8a7fded139c7b173a55d145e1eb0f7741
Anwesh43 <anweshthecool0@gmail.com>
2019-01-27 20:27:38 +0530

    adding LineToDoubleFStage class

--- LineToDoubleF.ts
+++ LineToDoubleF.ts
@@ -67,3 +67,33 @@ const drawLTDNode : Function = (context : CanvasRenderingContext2D, i : number, 
     }
     context.restore()
 }
+
+class LineToDoubleFStage {
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = backColor
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : LineToDoubleFStage = new LineToDoubleFStage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
+    }
+}
